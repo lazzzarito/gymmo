3fe0b5a739bb1d66a6fd63b3bbdc33f3f8a262df
1azarito <lazzzarito@gmail.com>
2025-12-23 02:01:38 -0500

    feat: add shop page and PixelCard component

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Onboarding from './components/Onboarding';
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 md:p-12 lg:p-24">
-      <Onboarding />
-    </main>
-  )
-}
